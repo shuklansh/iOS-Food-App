44cc413b09a008b1b51e579b9b2b838eefc26755
shuklansh <ansh.shukla270501@gmail.com>
2024-05-05 14:02:58 +0530

    Initial Commit

---
+++ iOS Food App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Food App
+//
+//  Created by Ansh Shukla on 05/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Food App/iOS_Food_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Food_AppApp.swift
+//  iOS Food App
+//
+//  Created by Ansh Shukla on 05/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Food_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
